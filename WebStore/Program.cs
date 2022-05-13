f37f72768cfc747f5c4652c7db6108d07d7d7729
Infarh <shmachilin@yandex.ru>
2022-05-13 20:19:37 +0300

    Настройка инфраструктуры системы MVC - сервисы + маршрут к контроллерам

--- WebStore/Program.cs
+++ WebStore/Program.cs
@@ -1,11 +1,17 @@
 var builder = WebApplication.CreateBuilder(args);
 
 // Конфигурирование перечня составных частей приложения
+builder.Services.AddControllersWithViews();
 
 var app = builder.Build();
 
 //var greetings = app.Configuration["ServerGreetings"];
 //app.MapGet("/", () => greetings);
-app.MapGet("/", () => app.Configuration["ServerGreetings"]);
+app.MapGet("/greetings", () => app.Configuration["ServerGreetings"]);
+
+//app.MapDefaultControllerRoute();
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
 
 app.Run();
