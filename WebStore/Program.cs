b8b60cf62491c989fa2131b69db5c9546a0b8c8b
Infarh <shmachilin@yandex.ru>
2022-05-17 19:53:33 +0300

    Хлам

--- WebStore/Program.cs
+++ WebStore/Program.cs
@@ -1,15 +1,11 @@
 var builder = WebApplication.CreateBuilder(args);
 
-// Конфигурирование перечня составных частей приложения
 builder.Services.AddControllersWithViews();
 
 var app = builder.Build();
 
-//var greetings = app.Configuration["ServerGreetings"];
-//app.MapGet("/", () => greetings);
 app.MapGet("/greetings", () => app.Configuration["ServerGreetings"]);
 
-//app.MapDefaultControllerRoute();
 app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}");
