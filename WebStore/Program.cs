441136e5eb052a6287f276792fee889f99644d35
Infarh <shmachilin@yandex.ru>
2022-05-20 20:00:20 +0300

    Пример соглашения

--- WebStore/Program.cs
+++ WebStore/Program.cs
@@ -1,8 +1,12 @@
+using WebStore.Infrastructure.Conventions;
 using WebStore.Infrastructure.Middleware;
 
 var builder = WebApplication.CreateBuilder(args);
 
-builder.Services.AddControllersWithViews();
+builder.Services.AddControllersWithViews(opt =>
+{
+    opt.Conventions.Add(new TestConvention());
+});
 
 var app = builder.Build();
 
