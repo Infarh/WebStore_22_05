e77dc786d94060593ebad23b7cc326bdbfd63ff0
Infarh <shmachilin@yandex.ru>
2022-05-20 20:28:47 +0300

    Заготовки действий контроллера сотрудников

--- WebStore/Controllers/EmployeesController.cs
+++ WebStore/Controllers/EmployeesController.cs
@@ -1,32 +1,36 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 
-using WebStore.Models;
+using WebStore.Services.Interfaces;
 
 namespace WebStore.Controllers;
 
 //[Route("Staff/{action=Index}/{Id?}")]
 public class EmployeesController : Controller
 {
-    private static readonly List<Employee> __Employees = new()
-    {
-        new Employee { Id = 1, LastName = "Иванов", FirstName = "Иван", Patronymic = "Иванович", Age = 23 },
-        new Employee { Id = 2, LastName = "Петров", FirstName = "Пётр", Patronymic = "Петрович", Age = 27 },
-        new Employee { Id = 3, LastName = "Сидоров", FirstName = "Сидор", Patronymic = "Сидорович", Age = 18 },
-    };
+    private readonly IEmployeesData _Employees;
+
+    public EmployeesController(IEmployeesData Employees) => _Employees = Employees;
 
     public IActionResult Index()
     {
-        return View(__Employees);
+        var employees = _Employees.GetAll();
+        return View(employees);
     }
 
     //[Route("Staff/Info/{Id}")]
     //[Route("[controller]/Info/{Id}")]
     public IActionResult Details(int Id)
     {
-        var employee = __Employees.FirstOrDefault(x => x.Id == Id);
+        var employee = _Employees.GetById(Id);
         if (employee is null)
             return NotFound();
 
         return View(employee);
     }
+
+    public IActionResult Create() => View();
+
+    public IActionResult Edit(int Id) => View();
+
+    public IActionResult Delete(int Id) => View();
 }
