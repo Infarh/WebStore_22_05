b8ba25219b3842a97d0d26c621e30bc4651fe1b2
Infarh <shmachilin@yandex.ru>
2022-05-24 20:24:32 +0300

    Заготовка визуальных компонентов

---
+++ WebStore/Components/SectionsViewComponent.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebStore.Components;
+
+//[ViewComponent(Name = "qwe")]
+public class SectionsViewComponent : ViewComponent
+{
+    //public async Task<IViewComponentResult> InvokeAsync() => View();
+    public IViewComponentResult Invoke() => View();
+}
