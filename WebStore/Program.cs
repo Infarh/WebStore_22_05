6a2d14eb6f93e3dc6231d3882f7e30d0430d3b2d
Infarh <shmachilin@yandex.ru>
2022-06-07 19:32:23 +0300

    Добавлена область Администратора

--- WebStore/Program.cs
+++ WebStore/Program.cs
@@ -96,8 +96,17 @@ app.MapGet("/greetings", () => app.Configuration["ServerGreetings"]);
 
 app.UseWelcomePage("/welcome");
 
-app.MapControllerRoute(
-    name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
+app.UseEndpoints(endpoints =>
+{
+    endpoints.MapControllerRoute(
+        name: "areas",
+        pattern: "{area:exists}/{controller=Home}/{action=Index}/{id?}"
+    );
+
+    endpoints.MapControllerRoute(
+        name: "default",
+        pattern: "{controller=Home}/{action=Index}/{id?}"
+    );
+});
 
 app.Run();
