f53f5354a17d4dbe7cb99e90f6f852aa9a62b1f0
Infarh <shmachilin@yandex.ru>
2022-06-17 21:04:22 +0300

    Заготовка клиента для WebAPI каталога товаров

---
+++ Services/WebStore.WebAPI.Clients/ProductsClient.cs
@@ -0,0 +1,41 @@
+﻿using WebStore.Domain;
+using WebStore.Domain.Entities;
+using WebStore.Interfaces.Services;
+using WebStore.WebAPI.Clients.Base;
+
+namespace WebStore.WebAPI.Clients;
+
+public class ProductsClient : BaseClient, IProductData
+{
+    public ProductsClient(HttpClient Client) : base(Client, "api/products") { }
+
+    public IEnumerable<Section> GetSections()
+    {
+        throw new NotImplementedException();
+    }
+
+    public Section? GetSectionById(int Id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public IEnumerable<Brand> GetBrands()
+    {
+        throw new NotImplementedException();
+    }
+
+    public Brand? GetBrandById(int Id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public IEnumerable<Product> GetProducts(ProductFilter? Filter = null)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Product? GetProductById(int Id)
+    {
+        throw new NotImplementedException();
+    }
+}
