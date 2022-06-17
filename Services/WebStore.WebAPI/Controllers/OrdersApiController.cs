712ec5e7c674f0ef2aa86b8dbceea9dde5278a17
Infarh <shmachilin@yandex.ru>
2022-06-17 21:14:50 +0300

    Заготовка контроллера для сервиса заказов

---
+++ Services/WebStore.WebAPI/Controllers/OrdersApiController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebStore.Interfaces.Services;
+
+namespace WebStore.WebAPI.Controllers;
+
+[ApiController]
+[Route("api/orders")]
+public class OrdersApiController : ControllerBase
+{
+    private readonly IOrderService _OrderService;
+    private readonly ILogger<OrdersApiController> _Logger;
+
+    public OrdersApiController(IOrderService OrderService, ILogger<OrdersApiController> Logger)
+    {
+        _OrderService = OrderService;
+        _Logger = Logger;
+    }
+}
