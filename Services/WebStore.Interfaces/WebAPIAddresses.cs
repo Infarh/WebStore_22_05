ca67e6fa080499a7319cb257c15fc0d898ffb22d
Infarh <shmachilin@yandex.ru>
2022-06-21 20:06:05 +0300

    Логика контроллера управления пользователями

--- Services/WebStore.Interfaces/WebAPIAddresses.cs
+++ Services/WebStore.Interfaces/WebAPIAddresses.cs
@@ -11,8 +11,8 @@ public static class WebAPIAddresses
 
         public static class Identity
         {
-            public const string Users = "api/users";
-            public const string Roles = "api/roles";
+            public const string Users = "api/v1/identity/users";
+            public const string Roles = "api/v1/identity/roles";
         }
     }
 }
