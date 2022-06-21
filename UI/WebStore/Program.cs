7d3f36234e36d2ed6ca7ee3ac20d77c8f5f3e88c
Infarh <shmachilin@yandex.ru>
2022-06-21 20:43:07 +0300

    Удаление БД из главного приложения

--- UI/WebStore/Program.cs
+++ UI/WebStore/Program.cs
@@ -21,25 +21,7 @@ var builder = WebApplication.CreateBuilder(args);
 var config = builder.Configuration;
 var services = builder.Services;
 
-//config.GetSection("DB")["Type"]
-//var db_type = config["DB:Type"];
-//var db_connection_string = config.GetConnectionString(db_type);
-
-//switch (db_type)
-//{
-//    case "DockerDB":
-//    case "SqlServer":
-//        services.AddDbContext<WebStoreDB>(opt => opt.UseSqlServer(db_connection_string));
-//        break;
-//    case "Sqlite":
-//        services.AddDbContext<WebStoreDB>(opt => opt.UseSqlite(db_connection_string, o => o.MigrationsAssembly("WebStore.DAL.Sqlite")));
-//        break;
-//}
-
-//services.AddScoped<DbInitializer>();
-
 services.AddIdentity<User, Role>(/*opt => { opt... }*/)
-   //.AddEntityFrameworkStores<WebStoreDB>()
    .AddDefaultTokenProviders();
 
 services.AddHttpClient("WebStoreAPIIdentity", client => client.BaseAddress = new(config["WebAPI"]))
