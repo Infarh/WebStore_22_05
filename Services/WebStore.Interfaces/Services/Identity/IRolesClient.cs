df187a4459222df9845f67e208130ca1921f7429
Infarh <shmachilin@yandex.ru>
2022-06-21 19:19:14 +0300

    Интерфейсы будущих клиентов WebAPI системы Identity

---
+++ Services/WebStore.Interfaces/Services/Identity/IRolesClient.cs
@@ -0,0 +1,9 @@
+using Microsoft.AspNetCore.Identity;
+using WebStore.Domain.Entities.Identity;
+
+namespace WebStore.Interfaces.Services.Identity;
+
+public interface IRolesClient : IRoleStore<Role>
+{
+
+}
