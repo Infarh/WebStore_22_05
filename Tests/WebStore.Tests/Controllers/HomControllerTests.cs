ccb0763e7ea1e4590c100345c5c2b79bf3717b0f
Infarh <shmachilin@yandex.ru>
2022-06-28 19:54:47 +0300

    Первый модульный тест

---
+++ Tests/WebStore.Tests/Controllers/HomControllerTests.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebStore.Controllers;
+
+using Assert = Xunit.Assert;
+
+namespace WebStore.Tests.Controllers;
+
+[TestClass]
+public class HomControllerTests
+{
+    [TestMethod]
+    public void Contacts_returns_with_View()
+    {
+        // AAA = A-A-A = Arrange - Act - Assert
+
+        #region Arrange
+        
+        var controller = new HomeController(null!);
+
+        #endregion
+
+        #region Act
+        
+        var result = controller.Contacts();
+
+        #endregion
+
+        #region Assert
+
+        var view_result = Assert.IsType<ViewResult>(result);
+
+        //Assert.Equal(nameof(HomeController.Contacts), view_result.ViewName); // неправильное утверждение
+        Assert.Null(view_result.ViewName);
+
+        #endregion
+    }
+}
