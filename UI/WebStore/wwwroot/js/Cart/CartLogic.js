bc51e4de1c0feaf1648058e7ccb5cf44614b446c
Infarh <shmachilin@yandex.ru>
2022-07-05 20:23:19 +0300

    Логика скрипта для кнопки "+" в представлении корзины

--- UI/WebStore/wwwroot/js/Cart/CartLogic.js
+++ UI/WebStore/wwwroot/js/Cart/CartLogic.js
@@ -13,7 +13,7 @@
 
     initEvents: function() {
         $(".add-to-cart").click(Cart.addToCart);
-        //$(".cart_quantity_up").click(Cart.incrementItem);
+        $(".cart_quantity_up").click(Cart.incrementItem);
         //$(".cart_quantity_down").click(Cart.decrementItem);
         //$(".cart_quantity_delete").click(Cart.removeItem);
     },
@@ -47,5 +47,53 @@
                 $("#cart-container").html(cartHtml);
             })
             .fail(function() { console.log("refreshCartView fail"); });
+    },
+
+    incrementItem: function(e) {
+        e.preventDefault();
+
+        const button = $(this);
+        const id = button.data("id");
+
+        const tr = button.closest("tr");
+
+        $.get(Cart._properties.addToCartLink + "/" + id)
+            .done(function(response) {
+                console.log(response.message);
+
+                const count = parseInt($(".cart_quantity_input", tr).val());
+                $(".cart_quantity_input", tr).val(count + 1);
+
+                Cart.refreshPrice(tr);
+                Cart.refreshCartView();
+            })
+            .fail(function() { console.log("incrementItem fail"); });
+    },
+
+    refreshPrice: function(tr) {
+        const count = parseInt($(".cart_quantity_input", tr).val());
+        const price = parseFloat($(".cart_price", tr).data("price"));
+
+        const totalPrice = price * count;
+
+        const value = totalPrice.toLocaleString("ru-RU", { style: "currency", currency: "RUB" });
+
+        const cartTotalPrice = $(".cart_total_price", tr);
+        cartTotalPrice.html(value);
+        cartTotalPrice.data("price", totalPrice);
+
+        Cart.refreshTotalPrice();
+    },
+
+    refreshTotalPrice: function() {
+        var totalPrice = 0;
+
+        $(".cart_total_price").each(function() {
+            const price = parseFloat($(this).data("price"));
+            totalPrice += price;
+        });
+
+        const value = totalPrice.toLocaleString("ru-RU", { style: "currency", currency: "RUB" });
+        $("#total-order-price").html(value);
     }
 }
