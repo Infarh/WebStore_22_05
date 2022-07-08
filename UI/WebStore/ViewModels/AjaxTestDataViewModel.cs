b9a2045efe88f02614dcfa8be8468c089bb83d1d
Infarh <shmachilin@yandex.ru>
2022-07-08 20:23:46 +0300

    Подготовка к тестированию ненавязчивого AJAX

---
+++ UI/WebStore/ViewModels/AjaxTestDataViewModel.cs
@@ -0,0 +1,10 @@
+﻿namespace WebStore.ViewModels;
+
+public class AjaxTestDataViewModel
+{
+    public int Id { get; init; }
+
+    public string? Message { get; init; }
+
+    public DateTime ServerTime { get; init; } = DateTime.Now;
+}
